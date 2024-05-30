878537cca5f7c3f1711f17c8b4b8b5aedd90e83a
engnandogabriel <eng.nandogabriel@gmail.com>
2024-05-30 13:29:54 -0300

    feat: first commit

---
+++ src/main/java/com/project/picpay/DesafioPicpayApplication.java
@@ -0,0 +1,13 @@
+package com.project.picpay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioPicpayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioPicpayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/picpay/DesafioPicpayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.picpay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioPicpayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
