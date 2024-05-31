70c16ebe1f811db95d8d855c06e46d4bad3f9cf9
engnandogabriel <eng.nandogabriel@gmail.com>
2024-05-31 12:49:17 -0300

    feat: added Name value object

---
+++ src/main/java/com/project/picpay/domain/value_objects/Name.java
@@ -0,0 +1,33 @@
+package com.project.picpay.domain.value_objects;
+
+public class Name {
+    private String name;
+
+    private Name(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) throws Exception {
+        this.name = validName(name);
+    }
+
+    public static Name create(String name) throws Exception {
+        name = validName(validName(name));
+        return new Name(name);
+    }
+
+    public static Name restore(String name) {
+        return new Name(name);
+    }
+
+    private static String validName(String name) throws Exception {
+        if (name.matches("[a-zA-Z\\s]+")) {
+            return name;
+        }
+        throw new Exception("Invalid name!");
+    }
+}
