bfd9f4782e647973fa578a483fbe449537015426
engnandogabriel <eng.nandogabriel@gmail.com>
2024-05-31 14:31:24 -0300

    fix: change amount in Payer and Payee Users to Double

--- src/main/java/com/project/picpay/domain/entities/user/Payee.java
+++ src/main/java/com/project/picpay/domain/entities/user/Payee.java
@@ -7,7 +7,7 @@ import com.project.picpay.domain.value_objects.Name;
 import java.util.UUID;
 
 public class Payee extends User {
-    private Payee(String id, String name, String document, String email, String password, String typeUser, Number amount) throws Exception{
+    private Payee(String id, String name, String document, String email, String password, String typeUser, Double amount) throws Exception{
         this.user_id = id;
         this.name = Name.create(name);
         this.document = Document.create(document);
@@ -16,12 +16,12 @@ public class Payee extends User {
         this.type_user = typeUser;
         this.amount = amount;
     }
-    public static Payee create(String name, String document, String email, String password, Number amount) throws Exception {
+    public static Payee create(String name, String document, String email, String password, Double amount) throws Exception {
         String id = UUID.randomUUID().toString();
         return new Payee(id, name, document, email, password, "payee", amount);
     }
 
-    public static Payee restore(String id, String name, String document, String email, String password, String typeUser, Number amount) throws Exception {
+    public static Payee restore(String id, String name, String document, String email, String password, String typeUser, Double amount) throws Exception {
         return new Payee(id, name, document, email, password, typeUser, amount);
     }
 }
