d76445b5506caabad609045d180d568b7833acc0
engnandogabriel <eng.nandogabriel@gmail.com>
2024-05-31 12:33:26 -0300

    feat: added test email value object

---
+++ src/test/java/com/project/picpay/domain/value_objects/EmailTest.java
@@ -0,0 +1,21 @@
+package com.project.picpay.domain.value_objects;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+public class EmailTest {
+
+    @Test
+    @DisplayName("Should be create a email valid")
+    void createEmail () throws Exception{
+        Email email = Email.create("johndoe@gmail.com");
+        Assertions.assertEquals("johndoe@gmail.com", email.getEmail());
+    }
+    @Test
+    @DisplayName("Should be throws a Exception: E-mail is invalid!")
+    void testeEmail() {
+        Exception exception = Assertions.assertThrows(Exception.class, () -> Email.create("invalid"));
+        Assertions.assertEquals("E-mail is invalid!", exception.getMessage());
+    }
+}
