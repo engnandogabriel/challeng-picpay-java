fdf363e6fd1fd9c4233f1e11e7403ee20c29dd53
engnandogabriel <eng.nandogabriel@gmail.com>
2024-05-31 18:15:59 -0300

    feat: added Transaction

--- src/main/java/com/project/picpay/domain/entities/Transaction.java
+++ src/main/java/com/project/picpay/domain/entities/Transaction.java
@@ -1,26 +1,64 @@
 package com.project.picpay.domain.entities;
 
-import com.project.picpay.domain.entities.user.Payee;
-import com.project.picpay.domain.entities.user.User;
+import com.project.picpay.domain.entities.user.Commun;
+import com.project.picpay.domain.entities.user.Merchant;
 
 import java.util.UUID;
 
 public class Transaction {
     private String tranfer_id;
-    private Number value;
+    private Double value;
     private String payer;
     private String payee;
 
-    private Transaction(String id, Number value, String payer, String payee){
+    private Transaction(String id, Double value, String payer, String payee) {
         this.tranfer_id = id;
         this.value = value;
         this.payer = payer;
         this.payee = payee;
     }
-    public static Transaction create(Number value, User payer, User payee) throws Exception{
+
+    public static Transaction create(Double value, Commun payer, Merchant payee) throws Exception {
         String id = UUID.randomUUID().toString();
-        if(value.doubleValue() <= 0) throw new Exception("Invalid amount!");
-//        if()
+        if (value <= 0) throw new Exception("Invalid amount!");
+        if (!payer.getType_user().equals("commun")) throw new Exception("Payer most be commun");
+        if (payer.getAmount() < value) {
+            throw new Exception("Insufficient payer amount");
+        }
+        payer.discountValue(value);
+        payee.addAmount(value);
         return new Transaction(id, value, payer.getUser_id(), payee.getUser_id());
+    }
+
+    public String getTranfer_id() {
+        return tranfer_id;
+    }
+
+    public void setTranfer_id(String tranfer_id) {
+        this.tranfer_id = tranfer_id;
+    }
+
+    public Number getValue() {
+        return value;
+    }
+
+    public void setValue(Double value) {
+        this.value = value;
+    }
+
+    public String getPayer() {
+        return payer;
+    }
+
+    public void setPayer(String payer) {
+        this.payer = payer;
+    }
+
+    public String getPayee() {
+        return payee;
+    }
+
+    public void setPayee(String payee) {
+        this.payee = payee;
     }
 }
