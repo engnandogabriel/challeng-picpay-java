44cbcd429f8b7419ca4ee348fb35b4b0228ba8c9
engnandogabriel <eng.nandogabriel@gmail.com>
2024-06-01 13:55:58 -0300

    test: added Exception in UserFactory to create users

--- src/main/java/com/project/picpay/domain/entities/user/UserFactory.java
+++ src/main/java/com/project/picpay/domain/entities/user/UserFactory.java
@@ -4,6 +4,6 @@ public class UserFactory {
     public static User create(String type, String name, String document, String email, String password, Double amount) throws Exception{
         if(type.equals("commun")) return Commun.create(name, document, email, password, amount);
         if(type.equals("merchant")) return Merchant.create(name, document, email, password, amount);
-        throw new Exception("");
+        throw new Exception("Invalid type user");
     }
 }
