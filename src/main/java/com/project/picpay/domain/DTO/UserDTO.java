169dfd9d502acdfc85da9fa3ea601e6d33503f45
engnandogabriel <eng.nandogabriel@gmail.com>
2024-06-01 13:56:45 -0300

    test: added type_user in UserDTO

--- src/main/java/com/project/picpay/domain/DTO/UserDTO.java
+++ src/main/java/com/project/picpay/domain/DTO/UserDTO.java
@@ -1,4 +1,4 @@
 package com.project.picpay.domain.DTO;
 
-public record UserDTO(String name, String document, String email, String password, Number value) {
+public record UserDTO(String type_user, String name, String document, String email, String password, Number value) {
 }
