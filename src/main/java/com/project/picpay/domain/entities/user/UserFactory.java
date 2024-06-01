4fcc90ad02150d2a12e8f516f49a2e24e9d951a0
engnandogabriel <eng.nandogabriel@gmail.com>
2024-06-01 14:57:20 -0300

    feat: added restore method in UserFactory

--- src/main/java/com/project/picpay/domain/entities/user/UserFactory.java
+++ src/main/java/com/project/picpay/domain/entities/user/UserFactory.java
@@ -1,9 +1,15 @@
 package com.project.picpay.domain.entities.user;
 
 public class UserFactory {
-    public static User create(String type, String name, String document, String email, String password, Double amount) throws Exception{
-        if(type.equals("commun")) return Commun.create(name, document, email, password, amount);
-        if(type.equals("merchant")) return Merchant.create(name, document, email, password, amount);
+    public static User create(String type, String name, String document, String email, String password, Double amount) throws Exception {
+        if (type.equals("commun")) return Commun.create(name, document, email, password, amount);
+        if (type.equals("merchant")) return Merchant.create(name, document, email, password, amount);
+        throw new Exception("Invalid type user");
+    }
+
+    public static User restore(String type, String id, String name, String document, String email, String password, Double amount) throws Exception {
+        if (type.equals("commun")) return Commun.restore(id, name, document, email, password, type, amount);
+        if (type.equals("merchant")) return Merchant.restore(id, name, document, email, password, type, amount);
         throw new Exception("Invalid type user");
     }
 }
