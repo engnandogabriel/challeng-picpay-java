5415ed611fd2ea80baa8f5288ef0a82901e54f17
engnandogabriel <eng.nandogabriel@gmail.com>
2024-06-03 11:15:12 -0300

    feat: added Handler class to handler in Services

---
+++ src/main/java/com/project/picpay/domain/HandlerService/Handlers.java
@@ -0,0 +1,18 @@
+package com.project.picpay.domain.HandlerService;
+
+import org.springframework.http.HttpStatus;
+
+public class Handlers<T> {
+    public HandlerDTO badRquest(Exception e){
+        return new HandlerDTO<String >(HttpStatus.BAD_REQUEST, "Bad Request", e.getMessage());
+    }
+    public HandlerDTO notFound(Exception e){
+        return new HandlerDTO<String>(HttpStatus.NOT_FOUND, "Not Found", e.getMessage());
+    }
+    public HandlerDTO servrError(Exception e){
+        return new HandlerDTO<String>(HttpStatus.INTERNAL_SERVER_ERROR, "ServerError", e.getMessage());
+    }
+    public HandlerDTO success(T obj){
+        return new HandlerDTO<T>(HttpStatus.OK, "Success", obj);
+    }
+}
