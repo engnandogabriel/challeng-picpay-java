4a007bc1295e5a56965783feab48a0881a3a3df6
engnandogabriel <eng.nandogabriel@gmail.com>
2024-06-05 12:14:36 -0300

    feat: added unauthorizedError exception

--- src/main/java/com/project/picpay/domain/HandlerService/Handlers.java
+++ src/main/java/com/project/picpay/domain/HandlerService/Handlers.java
@@ -3,16 +3,23 @@ package com.project.picpay.domain.HandlerService;
 import org.springframework.http.HttpStatus;
 
 public class Handlers<T> {
-    public HandlerDTO badRquest(Exception e){
-        return new HandlerDTO<String >(HttpStatus.BAD_REQUEST, "Bad Request", e.getMessage());
+    public HandlerDTO badRquest(Exception e) {
+        return new HandlerDTO<String>(HttpStatus.BAD_REQUEST, "Bad Request", e.getMessage());
     }
-    public HandlerDTO notFound(Exception e){
+
+    public HandlerDTO notFound(Exception e) {
         return new HandlerDTO<String>(HttpStatus.NOT_FOUND, "Not Found", e.getMessage());
     }
-    public HandlerDTO servrError(Exception e){
+
+    public HandlerDTO servrError(Exception e) {
         return new HandlerDTO<String>(HttpStatus.INTERNAL_SERVER_ERROR, "ServerError", "Internal Server Error: Please, try again later");
     }
-    public HandlerDTO success(T obj){
+
+    public HandlerDTO unauthorizedError(Exception e) {
+        return new HandlerDTO<String>(HttpStatus.UNAUTHORIZED, "UnauthorizedError", e.getMessage());
+    }
+
+    public HandlerDTO success(T obj) {
         return new HandlerDTO<T>(HttpStatus.OK, "Success", obj);
     }
 }
