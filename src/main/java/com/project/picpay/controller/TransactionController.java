808a7c238550036881a2a7cd70abc28143259fb7
engnandogabriel <eng.nandogabriel@gmail.com>
2024-06-05 14:06:12 -0300

    feat: added GetAllTransaction in TransactionController

--- src/main/java/com/project/picpay/controller/TransactionController.java
+++ src/main/java/com/project/picpay/controller/TransactionController.java
@@ -1,21 +1,28 @@
 package com.project.picpay.controller;
 
 import com.project.picpay.application.useCases.CreateTransaction;
+import com.project.picpay.application.useCases.GetAllTransaction;
 import com.project.picpay.domain.DTO.TransactionDTO;
 import com.project.picpay.domain.HandlerService.HandlerDTO;
+import com.project.picpay.domain.HandlerService.Handlers;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequestMapping(value = "transaction")
 @RestController
 public class TransactionController {
     private CreateTransaction createTransaction;
+    private GetAllTransaction getAllTransaction;
 
-    public TransactionController(CreateTransaction createTransaction) {
+    public TransactionController(CreateTransaction createTransaction, GetAllTransaction getAllTransaction) {
         this.createTransaction = createTransaction;
+        this.getAllTransaction = getAllTransaction;
+    }
+
+    @GetMapping(value = "")
+    public ResponseEntity<HandlerDTO> getAll() {
+        HandlerDTO output = this.getAllTransaction.execute();
+        return new ResponseEntity<>(output, output.status());
     }
 
     @PostMapping
