07f9ebb765696a96e985098e631d882238388f3f
engnandogabriel <eng.nandogabriel@gmail.com>
2024-06-06 16:23:08 -0300

    refactor: refactor: add error handler

--- src/main/java/com/project/picpay/infra/gateway/AuthorizationService.java
+++ src/main/java/com/project/picpay/infra/gateway/AuthorizationService.java
@@ -4,6 +4,8 @@ import com.project.picpay.domain.gateway.AuthorizationGateway;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.HttpServerErrorException;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.Map;
@@ -12,15 +14,26 @@ import java.util.Map;
 public class AuthorizationService implements AuthorizationGateway {
     private final String AUTHORIZATION_URL = "https://util.devi.tools/api/v2/authorize";
 
-    public AuthorizationService() {
-    }
-
     @Override
-    public boolean authorize(){
-        ResponseEntity<Map> authorizationResponse = new RestTemplate().getForEntity(this.AUTHORIZATION_URL, Map.class);
-        if (authorizationResponse.getStatusCode() == HttpStatus.OK)
-            if (authorizationResponse.getBody().get("status").equals("success"))
-                return true;
+    public boolean authorize() {
+        try {
+            ResponseEntity<Map> authorizationResponse = new RestTemplate().getForEntity(this.AUTHORIZATION_URL, Map.class);
+            if (authorizationResponse.getStatusCode() == HttpStatus.OK) {
+                Map<String, Object> responseBody = authorizationResponse.getBody();
+                if (responseBody != null && responseBody.containsKey("status")) {
+                    String status = (String) responseBody.get("status");
+                    return "success".equals(status);
+                }
+            }
+        } catch (HttpClientErrorException.Forbidden e) {
+            System.out.println("Acesso proibido: " + e.getMessage());
+            return false;
+        } catch (HttpServerErrorException.InternalServerError e) {
+            System.out.println("O serviço está temporariamente indisponível. Tente novamente mais tarde.");
+            return false;
+        } catch (Exception e) {
+            System.out.println("Ocorreu um erro ao tentar autorizar: " + e.getMessage());
+        }
         return false;
     }
 }
